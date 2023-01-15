b60e95100973f7fccd06329f881a0514e73acbc3
laPemaKu <113007919+laPemaKu@users.noreply.github.com>
2023-01-15 19:12:14 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string korisnik;
+            korisnik = Console.ReadLine();
+            korisnik = korisnik.Replace(" ", "_");
+            Console.WriteLine(korisnik);
+
+            Console.ReadKey();
+
+        }
+    }
+}
